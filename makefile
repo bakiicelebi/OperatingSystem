956989b33494f9304b5e062d093fa0409c338647
humeyraaozturk <humeyraaoztrk@gmail.com>
2024-12-26 20:34:55 +0300

    Redirection.c and Makefile Added

---
+++ makefile
@@ -0,0 +1,37 @@
+CC = gcc
+CFLAGS = -Wall -I./include
+
+# Tüm .c dosyalarını derleyip çalıştırılabilir dosyayı oluşturuyoruz
+all: shell
+
+# shell.o dosyasını önce oluşturuyoruz
+src/shell.o: src/shell.c include/shell.h
+	$(CC) $(CFLAGS) -c src/shell.c -o src/shell.o
+
+# execute.c dosyasını derliyoruz
+src/execute.o: src/execute.c include/shell.h
+	$(CC) $(CFLAGS) -c src/execute.c -o src/execute.o
+
+# redirection.c dosyasını derliyoruz
+src/redirection.o: src/redirection.c include/shell.h
+	$(CC) $(CFLAGS) -c src/redirection.c -o src/redirection.o
+
+# pipe.c dosyasını derliyoruz
+src/pipe.o: src/pipe.c include/shell.h
+	$(CC) $(CFLAGS) -c src/pipe.c -o src/pipe.o
+
+# background.c dosyasını derliyoruz
+src/background.o: src/background.c include/shell.h
+	$(CC) $(CFLAGS) -c src/background.c -o src/background.o
+
+# signal_handler.c dosyasını derliyoruz
+src/signal_handler.o: src/signal_handler.c include/shell.h
+	$(CC) $(CFLAGS) -c src/signal_handler.c -o src/signal_handler.o
+
+# shell çalıştırılabilirini oluşturuyoruz
+shell: src/shell.o src/execute.o src/redirection.o src/pipe.o src/background.o src/signal_handler.o
+	$(CC) $(CFLAGS) -o shell src/shell.o src/execute.o src/redirection.o src/pipe.o src/background.o src/signal_handler.o
+
+# Temizleme işlemi
+clean:
+	rm -f src/*.o shell
---
+++ src/redirection.c
@@ -0,0 +1,70 @@
+#include "shell.h"
+
+void handle_redirection(char *command)
+{
+    char *input_file = NULL;  // Giriş dosyası
+    char *output_file = NULL; // Çıkış dosyası
+    char *args[MAX_ARG_COUNT];
+    int i = 0;
+
+    char *token = strtok(command, " "); // Komutu boşluklara göre ayır
+    while (token != NULL)               // Token NULL olana kadar dön
+    {
+        if (strcmp(token, "<") == 0)
+        {
+            token = strtok(NULL, " ");
+            input_file = token;
+        }
+        else if (strcmp(token, ">") == 0)
+        {
+            token = strtok(NULL, " ");
+            output_file = token;
+        }
+        else
+        {
+            args[i++] = token;
+        }
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+
+    pid_t pid = fork(); // Yeni bir işlem oluştur
+    if (pid == 0)
+    {
+        if (input_file)
+        {
+            int fd_in = open(input_file, O_RDONLY); // Giriş dosyasını aç
+            if (fd_in < 0)
+            {
+                perror("Input file open failed");
+                exit(1);
+            }
+            dup2(fd_in, STDIN_FILENO);
+            close(fd_in);
+        }
+
+        if (output_file)
+        {
+            int fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd_out < 0)
+            {
+                perror("Output file open failed");
+                exit(1);
+            }
+            dup2(fd_out, STDOUT_FILENO);
+            close(fd_out);
+        }
+
+        execvp(args[0], args);
+        perror("execvp failed");
+        exit(1);
+    }
+    else if (pid > 0)
+    {
+        waitpid(pid, NULL, 0);
+    }
+    else
+    {
+        perror("fork failed");
+    }
+}
